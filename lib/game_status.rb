ded0b44eec85c72ee3a59f5bb7bd1e88a5c3aa1e
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-09 16:59:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,7 @@ end
     position_3 = board[win_index_3]
     
 if position_1 == "X" && position_2 == "X" && position_3 == "X" ||  position_1 == "O" && position_2 == "O" && position_3 == "O"
+  
     return win_combination
 else
     return false
