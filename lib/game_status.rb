3e697a91a16c7d06cb6c9f3ef1b6a151d1ed46cc
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-09 17:40:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ end
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
   
   def won?(board)
-   WIN_COMBINATIONS.each {|win_combination|
+   WIN_COMBINATIONS.each |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -19,11 +19,8 @@ end
     position_3 = board[win_index_3]
     
 if position_1 == "X" && position_2 == "X" && position_3 == "X" ||  position_1 == "O" && position_2 == "O" && position_3 == "O"
-  
     return win_combination
-
  end
-   }
   end
   
 def full?(board)
