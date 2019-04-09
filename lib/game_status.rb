d92b88deca8b3a7dc8f7401204d66f7e90181c13
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-09 16:37:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,6 @@ end
   
   def won?(board)
    WIN_COMBINATIONS.each do |win_combination|
-    
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -17,7 +16,6 @@ end
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combination
 else
